fd4c7d295d94c2dd82de032bae6cf41aedde06d6
NikulinOV <oleg309@yandex.ru>
2025-12-12 13:09:55 +0300

    Создана модель Friend с полями: имя, возраст, статус близости, дата знакомства, заметки

--- friends_api/models.py
+++ friends_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+    # Основные поля
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    age = models.IntegerField(verbose_name='Возраст', blank=True, null=True)
+    is_close = models.BooleanField(default=False, verbose_name='Близкий друг')
+    meeting_date = models.DateField(verbose_name='Дата знакомства', blank=True, null=True)
+    notes = models.TextField(verbose_name='Заметки', blank=True)
+
+    # Служебные поля (автоматически заполняются)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return self.name
