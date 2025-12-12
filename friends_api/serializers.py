a20ef3dbc621fc139051210d94e7f8e0e4aec32f
NikulinOV <oleg309@yandex.ru>
2025-12-12 14:34:10 +0300

    Создана папка templates/friends_api для HTML-шаблонов

--- friends_api/serializers.py
+++ friends_api/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Friend
 
-
 class FriendSerializer(serializers.ModelSerializer):
     class Meta:
         model = Friend
-        fields = ['id', 'name', 'age', 'is_close', 'meeting_date', 'notes', 'created_at', 'updated_at']
+        fields = '__all__'  # ВСЕ поля
--- friends_api/views.py
+++ friends_api/views.py
@@ -1,10 +1,21 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Friend
-from .serializers import FriendSerializer  # ← Импортируем сериализатор!
+from .serializers import FriendSerializer
 
-@api_view(['GET'])
+
+@api_view(['GET', 'POST'])
 def friend_list(request):
-    friends = Friend.objects.all()
-    serializer = FriendSerializer(friends, many=True)  # ← Используем сериализатор
-    return Response(serializer.data)
+    if request.method == 'GET':
+        friends = Friend.objects.all()
+        serializer = FriendSerializer(friends, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        print("Получены данные:", request.data)  # Для отладки
+        serializer = FriendSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
